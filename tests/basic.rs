835c6d708fc8679b61724889f35148a2d18b6a5b
Patrick Moriarty <pataroose@gmail.com>
2015-06-08 17:27:03 -0500

    Rust up, compiling at rustc 1.2.0-nightly (613e57b44 2015-06-01) (built 2015-06-02)

--- tests/basic.rs
+++ tests/basic.rs
@@ -7,22 +7,27 @@ extern crate resources_package_package;
 
 #[test]
 fn test() {
+    use std::path::{Path, PathBuf};
+
     static PACKAGE: resources_package_package::Package = resources_package!(
         "fixture"
     );
 
     assert_eq!(PACKAGE.iter().count(), 3);
 
-    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path == &Path::new("aaa.txt"))
-        .map(|(_, ctnt)| ctnt).as_ref(), Some(&b"aaa\naaa"));
+    // TODO: drop "[..]" when this bug is fixed:
++   //   https://github.com/rust-lang/rust/issues/22649
 
-    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path == &Path::new("b.txt"))
-        .map(|(_, ctnt)| ctnt).as_ref(), Some(&b"b b b"));
+    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path.as_path() == Path::new("aaa.txt"))
+        .map(|(_, ctnt)| ctnt), Some(&b"aaa\naaa"[..]));
 
-    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path == &Path::new("subdir").join("cc.txt"))
-        .map(|(_, ctnt)| ctnt).as_ref(), Some(&b"ccc"));
+    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path.as_path() == Path::new("b.txt"))
+        .map(|(_, ctnt)| ctnt), Some(&b"b b b"[..]));
 
-    assert_eq!(PACKAGE.find(&Path::new("aaa.txt")).as_ref(), Some(&b"aaa\naaa"));
-    assert_eq!(PACKAGE.find(&Path::new("b.txt")).as_ref(), Some(&b"b b b"));
-    assert_eq!(PACKAGE.find(&Path::new("subdir").join("cc.txt")).as_ref(), Some(&b"ccc"));
+    assert_eq!(PACKAGE.iter().find(|&(ref path, _)| path.as_path() == Path::new("subdir").join("cc.txt").as_path())
+        .map(|(_, ctnt)| ctnt), Some(&b"ccc"[..]));
+
+    assert_eq!(PACKAGE.find(Path::new("aaa.txt")), Some(&b"aaa\naaa"[..]));
+    assert_eq!(PACKAGE.find(Path::new("b.txt")), Some(&b"b b b"[..]));
+    assert_eq!(PACKAGE.find(Path::new("subdir").join("cc.txt").as_path()), Some(&b"ccc"[..]));
 }
